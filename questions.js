89c0a7870c6076bd83eeadd29ef32a70afa38dac
omwagh28 <151948718+omwagh28@users.noreply.github.com>
2024-02-07 21:16:50 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,46 @@
+let questions =[
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    Options:[
+        "A. Hyper Type Multi Language",
+        "B. Hyper Text Multiple Language",
+        "C. Hyper Text Markup Language",
+        "D. Home Text Multi Language"
+
+    ]
+    },
+    {
+        numb: 2,
+        question: "",
+        answer: "",
+        Options:[
+    
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        Options:[
+        
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        Options:[
+            
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        Options:[
+            
+        ]
+    },
+]
